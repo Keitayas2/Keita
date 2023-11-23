e696d1d9df1f0d4a22cddd98fc9a2b3e7faba839
山村圭汰 <yamamurakeita@MacBook-Air.local>
2023-11-23 21:38:50 +0900

    edit app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from fask import Flask, render_template
+from flask import Flask, render_template
 
-app = Flask(__main__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.reun(debug = True)
+    app.run(debug = True)
