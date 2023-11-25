e62c8a3f6eef6b1aa34fb76a489f1c2da7b74981
山村圭汰 <yamamurakeita@MacBook-Air.local>
2023-11-25 21:10:13 +0900

    edit all

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -12,7 +12,12 @@ def new():
 
 @app.route('/show', methods=["GET", "POST"])
 def show():
-    return render_template('show.html')
+    if request.method == 'GET':
+        return render_template('show.html')
+    elif request.method == 'POST':
+        text = request.form["input_text"]
+        print(text)
+        return render_template('show.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
