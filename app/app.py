00b2f237f0cb5deec6e80872de65d88aed4adf80
山村圭汰 <yamamurakeita@MacBook-Air.local>
2023-11-25 14:24:39 +0900

    edit all

--- app/app.py
+++ app/app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/new', methods=["GET", "POST"])
+def new():
+    return render_template('new.html')
+
+@app.route('/show', methods=["GET", "POST"])
+def show():
+    return render_template('show.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
