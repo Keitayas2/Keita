5c153e9f6ba18fb5448ac35dd56ca896d520d7f5
Keitayas2 <keita.y2004@icloud.com>
2023-12-05 10:43:09 +0900

    fixed

--- app/model.py
+++ app/model.py
@@ -6,13 +6,13 @@ def predict_player(text):
     file_name = '*.csv'
     result_dataframes = []
 
-
     csv_files = p.glob(file_name)
     for file in csv_files:
         df = pd.read_csv(file)
         # 選手名が1文字でない場合のみ部分一致でフィルタリング
         filtered_df = df[df['選手名'].str.contains(text)]
-        result_dataframes.append(filtered_df)
-        final_result = result_dataframes
+        if not filtered_df.empty:
+            result_dataframes.append(filtered_df)
 
+    final_result = pd.concat(result_dataframes, ignore_index=True) if result_dataframes else None
     return final_result
